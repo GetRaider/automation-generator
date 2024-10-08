07075a0a7f7db08ebd3eba1735878c1e21073003
Andrew Polovets <andrewpolovec@gmail.com>
2024-10-08 14:05:52 +0200

    Updated eslint ignore file

--- test-wdioV1/configs/mocha.config.js
+++ test-wdioV1/configs/mocha.config.js
@@ -1,5 +1,4 @@
 require("ts-node/register");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { configHelper } = require("../src/helpers/config/config.helper");
 
 module.exports = {
