46649974fada2f6157cdfc8517d5692c6c935883
Andrew Polovets <52605048+GetRaider@users.noreply.github.com>
2024-10-11 23:10:27 +0200

    Refactor: Jest framework (#11)
    * Added old framework version to change
    
    * Added config helper, and updated existing configs
    
    * Updated default env vars and html reporter config
    
    * Updated jest template
    
    * Removed test-jest project

--- templates/jest/src/fixtures/default-env-variables.fixture.ts
+++ templates/jest/src/fixtures/default-env-variables.fixture.ts
@@ -1,11 +1,14 @@
 export const defaultEnvVariables = `ENV=custom
 CI=false
 
-#Can be passed spec name/s or directory
+# Can be passed spec name/s
 SPEC_NAMES=get-all
+# Can be passed specs folder
+#SPECS_FOLDER_NAME=
 
-USER_PASSWORD=
 WEB_URL=
 API_URL=http://jsonplaceholder.typicode.com
+USER_PASSWORD=
 
+#TEST_RUNNER_TIMEOUT=90_000
 `;
